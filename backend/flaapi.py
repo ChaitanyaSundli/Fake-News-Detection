f9fbca3ef9ca36d0b0fc61ee5d7d5c5dfdc1eb4e
ChaitanyaSundli <130156770+ChaitanyaSundli@users.noreply.github.com>
2025-04-27 17:47:10 +0530

    Update flaapi.py

--- backend/flaapi.py
+++ backend/flaapi.py
@@ -158,6 +158,6 @@ def metrics():
 
 if __name__ == "__main__":
     import os
-    setup()  # Safe call
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
+
